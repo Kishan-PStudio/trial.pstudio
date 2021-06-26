c7634638eefb25cca2868be5bdfc67af6f1c2883
Kishan-PStudio <85757226+Kishan-PStudio@users.noreply.github.com>
2021-06-26 14:43:52 +0530

    Create trial.pages.login.js

---
+++ trial.pages.login.js
@@ -0,0 +1,24 @@
+function myFunction() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+    function validate()
+    {
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    if (username=="admin"&& password=="admin" 
+    || username=="kishan"&& password=="kishan"
+    || username=="raju"&& password=="raju")
+    {
+    alert("login sucessfull")
+    return false;
+    }
+    else 
+    {
+    alert("login failed")
+    }
+    }
